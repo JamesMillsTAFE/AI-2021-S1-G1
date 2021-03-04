6e9c589ad2ce141b51e4742d8b41bc7b7dbf708d
James Mills <james.mills39@tafensw.edu.au>
2021-03-04 12:57:43 +1100

    Update WanderStateUnity.cs

--- Assets/03 - StateMachineIntro/Scripts/WanderStateUnity.cs
+++ Assets/03 - StateMachineIntro/Scripts/WanderStateUnity.cs
@@ -6,25 +6,25 @@ public class WanderStateUnity : StateMachineBehaviour
 {
     // Like the start function of MonoBehaviour except it runs only the first frame update is called
     // in this state
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateEnter(Animator _animator, AnimatorStateInfo _stateInfo, int _layerIndex)
     {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
+        base.OnStateEnter(_animator, _stateInfo, _layerIndex);
     }
 
     // Similar to MonoBehaviour Update method, runs once per update frame
     // EXCEPT the first and last frame that it is in the state for
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateUpdate(Animator _animator, AnimatorStateInfo _stateInfo, int _layerIndex)
     {
-        base.OnStateUpdate(animator, stateInfo, layerIndex);
+        base.OnStateUpdate(_animator, _stateInfo, _layerIndex);
     }
 
     // Like the start function of MonoBehaviour except it runs only the last frame update is called
     // in this state
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateExit(Animator _animator, AnimatorStateInfo _stateInfo, int _layerIndex)
     {
-        base.OnStateExit(animator, stateInfo, layerIndex);
+        base.OnStateExit(_animator, _stateInfo, _layerIndex);
 
         // Transition to new state
-        animator.SetTrigger("BeginTarget");
+        _animator.SetTrigger("BeginTarget");
     }
 }
