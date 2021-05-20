ac33dc96b942be5e8e5871d20655a2dbcb3214ce
JamesMillsTAFE <james.mills39@tafensw.edu.au>
2021-05-20 15:02:54 +1000

    Statemachines with Delegates

--- Assets/03 - StateMachineIntro/Scripts/CustomStateMachine.cs
+++ Assets/03 - StateMachineIntro/Scripts/CustomStateMachine.cs
@@ -2,101 +2,104 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum State
+namespace OldStateMachines
 {
-    Wander,
-    Target,
-    Attack,
-    Damage,
-    Die
-}
-
-public class CustomStateMachine : MonoBehaviour
-{
-    private Dictionary<State, string> stateCoroutines = new Dictionary<State, string>();
-
-    private Coroutine currentCoroutine;
-
-    // Start is called before the first frame update
-    void Start()
+    public enum State
     {
-        // Specify which states map to which functions
-        //stateCoroutines.Add(State.Wander, "WanderState");
+        Wander,
+        Target,
+        Attack,
+        Damage,
+        Die
+    }
 
-        // This loops through until we hit the die state
-        for (int i = 0; i <= (int)State.Die; i++)
+    public class CustomStateMachine : MonoBehaviour
+    {
+        private Dictionary<State, string> stateCoroutines = new Dictionary<State, string>();
+
+        private Coroutine currentCoroutine;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            // Convert the iterator to a state
-            State state = (State)i;
-            // Construct the string name
-            string functionName = state.ToString() + "State";
+            // Specify which states map to which functions
+            //stateCoroutines.Add(State.Wander, "WanderState");
 
-            // Add the states to the dictionary
-            stateCoroutines.Add(state, functionName);
+            // This loops through until we hit the die state
+            for (int i = 0; i <= (int)State.Die; i++)
+            {
+                // Convert the iterator to a state
+                State state = (State)i;
+                // Construct the string name
+                string functionName = state.ToString() + "State";
 
-            Debug.Log(state.ToString() + ", " + functionName);
+                // Add the states to the dictionary
+                stateCoroutines.Add(state, functionName);
+
+                Debug.Log(state.ToString() + ", " + functionName);
+            }
+
+            SwapState(State.Wander);
         }
 
-        SwapState(State.Wander);
-    }
-
-    public void SwapState(State _newState)
-    {
-        // Is there a coroutine already running?
-        if(currentCoroutine != null)
+        public void SwapState(State _newState)
         {
-            // There is, so stop it and make currentCoroutine null
-            StopCoroutine(currentCoroutine);
-            currentCoroutine = null;
+            // Is there a coroutine already running?
+            if (currentCoroutine != null)
+            {
+                // There is, so stop it and make currentCoroutine null
+                StopCoroutine(currentCoroutine);
+                currentCoroutine = null;
+            }
+
+            // Runs the coroutine, Use the function version most of the time.
+            // Stores which coroutine we are currently running so we can stop it later
+            currentCoroutine = StartCoroutine(stateCoroutines[_newState]);
         }
 
-        // Runs the coroutine, Use the function version most of the time.
-        // Stores which coroutine we are currently running so we can stop it later
-        currentCoroutine = StartCoroutine(stateCoroutines[_newState]);
-    }
+        private IEnumerator WanderState()
+        {
+            Debug.Log("This is before the delay!");
 
-    private IEnumerator WanderState()
-    {
-        Debug.Log("This is before the delay!");
+            yield return new WaitForSecondsRealtime(1);
 
-        yield return new WaitForSecondsRealtime(1);
-        
-        Debug.Log("This is after the delay!");
-    }
+            Debug.Log("This is after the delay!");
+        }
 
-    private IEnumerator TargetState()
-    {
-        Debug.Log("This is before the delay!");
+        private IEnumerator TargetState()
+        {
+            Debug.Log("This is before the delay!");
 
-        yield return new WaitForSecondsRealtime(1);
+            yield return new WaitForSecondsRealtime(1);
 
-        Debug.Log("This is after the delay!");
-    }
+            Debug.Log("This is after the delay!");
+        }
 
-    private IEnumerator AttackState()
-    {
-        Debug.Log("This is before the delay!");
+        private IEnumerator AttackState()
+        {
+            Debug.Log("This is before the delay!");
 
-        yield return new WaitForSecondsRealtime(1);
+            yield return new WaitForSecondsRealtime(1);
 
-        Debug.Log("This is after the delay!");
-    }
+            Debug.Log("This is after the delay!");
+        }
 
-    private IEnumerator DamageState()
-    {
-        Debug.Log("This is before the delay!");
+        private IEnumerator DamageState()
+        {
+            Debug.Log("This is before the delay!");
 
-        yield return new WaitForSecondsRealtime(1);
+            yield return new WaitForSecondsRealtime(1);
 
-        Debug.Log("This is after the delay!");
-    }
+            Debug.Log("This is after the delay!");
+        }
 
-    private IEnumerator DieState()
-    {
-        Debug.Log("This is before the delay!");
+        private IEnumerator DieState()
+        {
+            Debug.Log("This is before the delay!");
 
-        yield return new WaitForSecondsRealtime(1);
+            yield return new WaitForSecondsRealtime(1);
 
-        Debug.Log("This is after the delay!");
+            Debug.Log("This is after the delay!");
+        }
     }
 }
---
+++ Assets/14 - State Machines/Scripts/StateMachine.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateMachines
+{
+    public enum States
+    {
+        Translate,
+        Rotate,
+        Scale
+    }
+
+    // The delegate that dictates what the functions for each state will look like.
+    public delegate void StateDelegate();
+
+    public class StateMachine : MonoBehaviour
+    {
+        private Dictionary<States, StateDelegate> states = new Dictionary<States, StateDelegate>();
+
+        [SerializeField] private States currentState = States.Translate;
+        [SerializeField] private Transform controlled; // The thing that will be affected by our statemachine
+        [SerializeField] private float speed = 1f; // This isn't really in a statemachine, this is just for testing
+
+        // This is used to change states from anywhere within the code that has reference
+        // to the StateMachine
+        public void ChangeState(States _newState)
+        {
+            if(_newState != currentState)
+                currentState = _newState;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // This is the same as checking if the variable is null, then setting it, otherwise
+            // retain the value
+            controlled ??= transform;
+
+            states.Add(States.Translate, Translate);
+            states.Add(States.Rotate, Rotate);
+            states.Add(States.Scale, Scale);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // These two lines are what actually runs the state machine.
+            // It works by attempting to retrieve the relevate function for the current state,
+            // then running the function if it successfully found it.
+            if(states.TryGetValue(currentState, out StateDelegate state))
+                state.Invoke();
+            else
+                Debug.LogError($"No state function set for state {currentState}.");
+        }
+
+        // The function that will run when we are in the Translate state.
+        private void Translate() => controlled.position += controlled.forward * Time.deltaTime * speed;
+        private void Rotate() => controlled.Rotate(Vector3.up, speed * .5f);
+        private void Scale() => controlled.localScale += Vector3.one * Time.deltaTime * speed;
+    }
+}
